83c9861db2d573d74224e7afac971f3112f886df
Christopher Greaves <chgreaves123@gmail.com>
2024-08-14 03:51:10 -0400

    flask changes

--- main.py
+++ main.py
@@ -2,27 +2,101 @@ from dotenv import load_dotenv
 import os
 import base64
 import json
-from requests import post
+import requests
+import urllib.parse
+from datetime import datetime
+import calendar
+from flask import Flask, redirect, request, jsonify, session
+from requests import post, get
+
+app = Flask(__name__)
+app.secret_key = ""
+
+@app.route('/')
+def index():
+    return "Welcome to Discog Me <a href='/login'>Login with Spotify</a>"
+
+@app.route('/login')
+def login():
+    scope = 'user-read-private user-read email'
+
+    params = {
+        'client_id': client_id,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': redirect_uri,
+        'show_dialog': True, #force user to login every time
+    }
+
+    AUTH_URL = f"{auth_url}?{urllib.parse.urlencode(params)}"
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in requests.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in requests.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': redirect_uri,
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+    response = requests.post(token_url, data=req_body)
+    token_info = response.json()
+
+    session['access_token'] = token_info['access_token']
+    session['refresh_token'] = token_info['refresh_token']
+    session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+    
+    return redirect('/playlists')
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+    response = requests.post(token_url, data=req_body)
+    new_token_info = response.json()
+
+    session['access_token'] = new_token_info['access_token']
+    session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+
+    return redirect('/albums')
+
 
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
+redirect_uri = "http://localhost:3000/callback"
+
+auth_url = "https:accounts.spotify.com/authorize"
+token_url = "https:accounts.spotify.com/api/token"
+api_base_url = "https://api.spotify.com/v1/"
 
 # print(client_id, client_secret)
 
-def get_token():
+'''def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-    url = "https://accounts.spotify.com/api/token"
     headers = {
         "Authorization" : "Basic " + auth_base64,
         "Content-Type" : "application/x-www-form-urlencoded"
     }
     data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
+    result = post(token_url, headers=headers, data=data)
     json_result = json.loads(result.content)
     token = json_result["access_token"]
     return token
@@ -33,4 +107,4 @@ def get_auth_header(token):
 
 token = get_token()
 print(token)
-    
+    '''
